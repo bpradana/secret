44618478d469ce248fb37fa633d31c2eef3afc6f
jagad <elarjeigidigiem@gmail.com>
2021-04-25 23:54:20 +0700

    Create dumy data from models & assets

---
+++ lib/data/users.dart
@@ -0,0 +1,67 @@
+import 'package:secret/model/user.dart';
+
+final dummyUsers = [
+  User(
+    workPlace: 'Universitas Sebelas Maret',
+    gender: 'Cewek',
+    bio:
+        'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    age: 22,
+    likes: 5761,
+    imgUrl: 'assets/user1.jpg',
+  ),
+  User(
+    workPlace: 'SMAN 1 Karanganyar',
+    gender: 'Cowok',
+    bio:
+        'Travelling, adventures, extreme sports are also an integral part of me, but I like watching and admiring extreme sports rather than doing it ?',
+    age: 18,
+    likes: 211,
+    imgUrl: 'assets/user2.jpg',
+  ),
+  User(
+    workPlace: 'Universitas Gadjah Mada',
+    gender: 'Cewek',
+    bio:
+        'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    age: 21,
+    likes: 387,
+    imgUrl: 'assets/user3.jpg',
+  ),
+  User(
+    workPlace: 'Institut Pertanian Bogor',
+    gender: 'Cowok',
+    bio:
+        'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    age: 22,
+    likes: 667,
+    imgUrl: 'assets/user4.jpg',
+  ),
+  User(
+    workPlace: 'SMAN 2 Surakarta',
+    gender: 'Cewek',
+    bio:
+        'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    age: 18,
+    likes: 98721,
+    imgUrl: 'assets/user5.jpg',
+  ),
+  User(
+    workPlace: 'Institut Seni Surakarta',
+    gender: 'Cewek',
+    bio:
+        'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    age: 21,
+    likes: 88,
+    imgUrl: 'assets/user6.jpg',
+  ),
+  User(
+    workPlace: 'Universitas Gadjah Mada',
+    gender: 'Cewek',
+    bio:
+        'Psychology, science, and art are what helps me to learn the outside world and myself.',
+    age: 21,
+    likes: 210100,
+    imgUrl: 'assets/user7.jpg',
+  ),
+];
